a5f42a426e5e13a5d132b85fa3de732bd6337875
Christina Braam <rozeblom@live.com>
2023-03-30 14:06:24 +0200

    Ik snap er niks van

---
+++ algemene_functies.py
@@ -0,0 +1,9 @@
+def mijn_functie_1(mijn_int):
+    return mijn_int**2
+
+print(mijn_functie_1(10))
+
+def mijn_functie_2(a,b):
+    return [a+b, a-b, a*b, a/b]
+
+print(mijn_functie_2(100,20))
---
+++ reclame.py
@@ -0,0 +1,49 @@
+smaak = "aardbei"
+prijs = 4
+korting = prijs - prijs*0.1
+aanbieding_1 = (f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {korting}")
+print(aanbieding_1)
+
+def inkomsten_totaal():
+    string = "{a}","{b}","{c}","{d}","{e}","{f}","{g}, "{h}"
+    h = float
+    bedrag = h
+    totaal = a+b+c+d+e+f+g
+return totaal
+    totaal = Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {bedrag} euro btw betaald dient te worden.
+
+def laag_en_hoog(mijn_lijst):
+    mijn_lijst = "{a}", "{b}", "{c}", "{d}", "{e}", "{f}", "{g}"
+return max en min
+
+print(laag_en_hoog)
+
+def gemiddelde(mijn_lijst):
+    mijn_lijst= "{a}", "{b}", "{c}", "{d}", "{e}", "{f}", "{g}"
+    bedrag = (a+b+c+d+e+f+g)/2
+return "De gemiddelde inkomsten deze week zijn {bedrag} euro."
+
+print(gemiddelde)
+
+def meervoudig(invoer_lijst):
+    a[5:10]
+    a = int
+return max en min
+hoog_en_laag()= max en min
+
+def combinatie(invoer_lijst_2):
+    meervoudig(invoer_lijst_2)
+return = korte_lijst
+return mijn_functie2(korte_lijst)
+
+
+
+
+
+
+
+
+
+
+
+
