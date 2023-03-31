46eb9e4c0b8a7e0f6140870a42b12f8f13cf06bd
Christina Braam <rozeblom@live.com>
2023-03-31 17:04:34 +0200

    Nog meer gewijzigd

--- presentatie.py
+++ presentatie.py
@@ -1,10 +1,10 @@
 def presenteer(dictionary, totaal):
     dictionary = {
-"Aardbeien-ijs-totaal" : 1000,
-"Vanille-ijs-totaal" : 2000,
-"Chocolade-ijs-totaal" : 1500,
-"Waterijsjes-totaal" : 750
-}
+    "Aardbeien-ijs-totaal" : 1000,
+    "Vanille-ijs-totaal" : 2000,
+    "Chocolade-ijs-totaal" : 1500,
+    "Waterijsjes-totaal" : 750
+    }
     totaal = 5250
     uitvoer = dictionary , "=" *25 , totaal
     return uitvoer
